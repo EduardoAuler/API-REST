f3eb5edfc76d4429a62ee2a3623bc0b246c42c9d
Borderless Dev <dev@borderless.com>
2026-02-10 15:49:58 -0300

    feat: adiciona responseEntity, delete e get id

--- API-REST/src/main/java/com/apirest/API/REST/controller/UserController.java
+++ API-REST/src/main/java/com/apirest/API/REST/controller/UserController.java
@@ -5,6 +5,8 @@ import com.apirest.API.REST.dto.UserDTO;
 import com.apirest.API.REST.mapper.UserMapper;
 import com.apirest.API.REST.model.User;
 import com.apirest.API.REST.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,17 +24,36 @@ public class UserController {//fala com DTO
     }
 
     @GetMapping("/users")
-    public List<UserDTO> findAll(){
-        return service.getAll().stream()
+    public ResponseEntity<List<UserDTO>> findAll(){
+        List<UserDTO> users = service.getAll().stream()
                 .map(mapper::toDTO)
                 .toList();
+
+        return ResponseEntity.ok(users);
+    }
+
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable long id){
+        UserDTO user = mapper.toDTO(service.findUserById(id));
+
+        return ResponseEntity.ok(user);
     }
 
     @PostMapping("/users")
-    public UserDTO createUser(@RequestBody UserDTO dto){
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO dto){
         User user = mapper.toEntity(dto);
         User saved = service.createUser(user);
+        UserDTO response = mapper.toDTO(saved);
 
-        return mapper.toDTO(saved);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable long id){
+        service.deleteUser(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
